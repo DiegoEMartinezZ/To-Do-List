2b33f3ec7b69f91d53c41d90fc93bd5fa5a3427b
DiegoEMartinezZ <diermaza.23@gmail.com>
2023-01-06 11:00:20 -0500

    se agrego que al momento de revisar la app muestre el dia y la fecha en que se esta revisando la app

--- src/pages/mainPage/MainPage.jsx
+++ src/pages/mainPage/MainPage.jsx
@@ -1,19 +1,38 @@
 import React from 'react';
 import './mainPage.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 const MainPage = () => {
+  let date = new Date();
+
+  const numberDay = {
+    1: 'Domingo',
+    2: 'Lunes',
+    3: 'Martes',
+    4: 'Miercoles',
+    5: 'Jueves',
+    6: 'Viernes',
+    7: 'Sabado',
+  };
+
+  let spanishNameDay = numberDay[date.getDate()];
+
   return (
     <>
       <div className="main-date-container">
-        <section className="hamburguer-menu">
-          <div> Boton de menu </div>
-        </section>
-        <section className="date-container">
-          <h2 className="name-day"> Viernes </h2>
-          <h3 className="exact-day"> Fecha del dia de hoy </h3>
-        </section>
-        <section className="time-container">
-          <h1 className="time"> HORA </h1>
+        <nav className="hamburguer-menu">
+          <FontAwesomeIcon icon={faBars} />
+        </nav>
+
+        <section className="current-date-container">
+          <div className="date-container">
+            <h2 className="name-day"> {spanishNameDay}</h2>
+            <h3 className="exact-day"> {date.toLocaleDateString()} </h3>
+          </div>
+          <div className="time-container">
+            <h1 className="time"> API Clock</h1>
+          </div>
         </section>
       </div>
     </>
