0b24f4a2af4e2d10eacc1536f6b1314c22ec686e
DiegoEMartinezZ <diermaza.23@gmail.com>
2023-05-29 11:41:51 -0500

    se quita la posibilidad de mover los dias debido a problemas al finalizar mes, mas adelante se trabajara en la solucion al problema

--- src/components/calendar/Calendar.jsx
+++ src/components/calendar/Calendar.jsx
@@ -1,9 +1,4 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faCircleChevronLeft,
-  faCircleChevronRight,
-} from "@fortawesome/free-solid-svg-icons";
 
 import { useContext } from "react";
 import { CalendarContext } from "../../resources/calendarContext/CalendarContext";
@@ -32,15 +27,9 @@ export const Calendar = () => {
 
   return (
     <main className="current-day-container">
-      <button className="arrow-calendar-left" onClick={previousDay}>
-        <FontAwesomeIcon icon={faCircleChevronLeft} className="arrow-left" />
-      </button>
       <h3 className="current-day-name">
         {spanishNameDayOfWeek[currentDayOfWeek]} {currentDay}
       </h3>
-      <button className="arrow-calendar-right" onClick={nextDay}>
-        <FontAwesomeIcon icon={faCircleChevronRight} className="arrow-right" />
-      </button>
     </main>
   );
 };
