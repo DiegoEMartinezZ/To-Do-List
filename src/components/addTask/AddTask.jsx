d3fbe051b5ff1582932c0b0e7b877f571c6fbfac
DiegoEMartinezZ <diermaza.23@gmail.com>
2023-06-14 19:54:36 -0500

    se agrega boton para minimizar la ventana de crear tarea, se evalua opcion de quitar el texto de que no hay tareas colocadas para que solo quede el boton de agregar tarea

--- src/components/addTask/AddTask.jsx
+++ src/components/addTask/AddTask.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { IconPlus, IconChevronUp } from "@tabler/icons-react";
 import "./addTask.css";
-import { IconPlus } from "@tabler/icons-react";
 
 const AddTask = () => {
+  const closeTaskWindowHandler = () => {
+    const addTaskWindow = document.querySelector(".add-task-container");
+    addTaskWindow.classList.toggle("active");
+  };
+
   return (
     <section className="add-task-container">
       <form action="submit" className="form-add-task">
+        <IconChevronUp
+          className="close-add-window"
+          onClick={closeTaskWindowHandler}
+        />
         <h1 className="title-add-task-container">
           <FontAwesomeIcon icon={faStar} className="favorite-icon" /> Nueva
           Tarea
